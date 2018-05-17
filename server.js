9ab2310a6f5fecc46182d64c0cfc23741c8a3e8d
Glitch (ms-imagesearchapi) <none>
2018-05-17 17:11:17 +0000

    ⛱🕌 Checkpoint ./public/style.css:1118247/964 ./public/client.js:1118247/1270 ./views/index.html:1118247/719 ./server.js:1118247/1535

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
-// server.js
-// where your node app starts
 
 // init project
 var express = require('express');
 var app = express();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -14,6 +11,20 @@ app.use(express.static('public'));
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
+});
+
+
+app.get("/api/imagesearch/:search",(request, response)=>{
+  var search = request.params.search;
+  
+  response.write(
+  "<script> (function() {var cx = '003066421765772510641:fqbs-hzafsq'; var gcse = document.createElement('script'); gcse.type = 'text/javascript'; gcse.async = true; gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;" +
+    "var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gcse, s); })(); </script> <gcse:search></gcse:search>");
+  
+  
+  
+  
+  
 });
 
 // listen for requests :)

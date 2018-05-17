2fe5c87daefc51927c685c057545b5e2efccffca
Glitch (ms-imagesearchapi) <none>
2018-05-17 17:52:50 +0000

    🚥🎊 Checkpoint ./server.js:1118247/1383

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 // init project
 var express = require('express');
 var app = express();
-var request = require("request");
+
 
 
 
@@ -20,18 +20,15 @@ app.get("/api/imagesearch/:search",(request, response)=>{
   search = search.replace(/\s/g,"+");
   var cx = '003066421765772510641:fqbs-hzafsq';
   var key = "AIzaSyC1-YQaSgU9Evazx36rCrtB_py6azRTvow";
+  var request = require('request');
   
-  request('https://www.googleapis.com/customsearch/v1?q='+search+"&cx="+cx+"&key="+key, { json: true }, (err, res, body) => {
+request('https://www.googleapis.com/customsearch/v1?q='+search+"&cx="+cx+"&key="+key, { json: true }, (err, res, body) => {
   if (err) { return console.log(err); }
-  
-  console.log(body.explanation);
+  response.send(body.body);
 });
-  
-  
-  
-  
-  //GET https://www.googleapis.com/customsearch/v1?q=cute+dogs&cx=003066421765772510641%3Afqbs-hzafsq&key={YOUR_API_KEY}
 
+  
+ 
 });
 
 // listen for requests :)

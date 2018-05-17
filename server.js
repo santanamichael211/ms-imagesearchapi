d42765339a7c79bf298426d7fdb9db7c4158d484
Glitch (ms-imagesearchapi) <none>
2018-05-17 19:48:39 +0000

    ⛱❄️ Checkpoint ./server.js:1118247/4003

--- server.js
+++ server.js
@@ -25,13 +25,14 @@ app.get("/api/imagesearch/:search",(request, response)=>{
   var url = 'https://www.googleapis.com/customsearch/v1?q='+search+"&cx="+cx+"&key="+key;
   
 getConnection(url).then(function(data){
-  getFormattedA
-  
+  getFormattedArr(data).then(function(res){
+    response.send(res);
+  }).catch(function(err){
+   if(err)console.log(err);
+  });
 }).catch(function(err){
 if(err)console.log(err);
 });
-
-  
  
 });
 
@@ -44,12 +45,10 @@ var listener = app.listen(process.env.PORT, function () {
 function getConnection(url){
   return new Promise(function(reject,resolve){
     var request = require('request');
-    
         request(url, { json: true }, (err, res, body) => {
                 if (err) { reject(err); }
                 else{ resolve(body.items);}
         });
-    
 });
 }
 
@@ -62,7 +61,7 @@ function getFormattedArr(data){
       
       var formattedArr = [];
       var arrLen = data.length;
-      
+     
       for(var i = 0; i<arrLen; i++){
       formattedArr.push({
       url: data[i].pagemap.cse_image[i].src 
@@ -70,8 +69,10 @@ function getFormattedArr(data){
       //context: resultsArray[i].link,
       //thumbnail: resultsArray[i].pagemap.cse_thumbnail[i].src 
       })
-      console.log(formattedArr[i]);
+      
     }
+    
+      //console.log(formattedArr);
       resolve(formattedArr);
     }
   });

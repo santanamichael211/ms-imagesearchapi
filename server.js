ff7d3a5e9deefd4969e3678d79b216562c2730bf
Glitch (ms-imagesearchapi) <none>
2018-05-17 20:19:44 +0000

    👚💂 Checkpoint ./server.js:1118247/293

--- server.js
+++ server.js
@@ -25,9 +25,9 @@ app.get("/api/imagesearch/:search",(request, response)=>{
   var url = 'https://www.googleapis.com/customsearch/v1?q='+search+"&cx="+cx+"&key="+key;
   
 getConnection(url).then(function(data){
-  
+  response.send("Hello")
 }).catch(function(err){
-//console.log(err);
+//response.send(err);
 });
  
 });
@@ -42,8 +42,7 @@ function getConnection(url){
   return new Promise(function(reject,resolve){
     var request = require('request');
         request(url, { json: true }, (err, res, body) => {
-                if (err) { reject(err); }
-                else{ console.log(url); resolve(body.items);}
+                console.log(url); resolve(body.items);
         });
 });
 }

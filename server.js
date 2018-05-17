c10933c9b584375bedcc6c369770c35fc2f9f9a6
Glitch (ms-imagesearchapi) <none>
2018-05-17 19:28:07 +0000

    👮💏 Checkpoint ./server.js:1118247/1280

--- server.js
+++ server.js
@@ -22,19 +22,19 @@ app.get("/api/imagesearch/:search",(request, response)=>{
   search = search.replace(/\s/g,"+");
   var cx = '003066421765772510641:fqbs-hzafsq';
   var key = "AIzaSyC1-YQaSgU9Evazx36rCrtB_py6azRTvow";
-  var request = require('request');
+  var url = 'https://www.googleapis.com/customsearch/v1?q='+search+"&cx="+cx+"&key="+key;
   
 request('https://www.googleapis.com/customsearch/v1?q='+search+"&cx="+cx+"&key="+key, { json: true }, (err, res, body) => {
   if (err) { return console.log(err); }
   var resultsArray = body.items;
   
   //response.send(resultsArray[0].pagemap.cse_thumbnail[0].src);
-  
-  
 
-  setObjArr(resultsArray).then(res =>{
-  response.send("hello");
-  })
+  setObjArr(resultsArray).then((data)=>{
+  response.send(data);
+  }).catch(function(err){
+    console.log(err);
+  });
 
     //response.send(finalResults);
   
@@ -50,26 +50,38 @@ var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
-function setObjArr(resultsArray){
+function setObjArr(resultsArray, url){
   return new Promise((resolve, reject) => {
-    if(!resultsArray){reject();}
+    var request = require('request');
+    
+    request
+    
+    
+    
+  });
+}
+/*
+
+if(!resultsArray){reject("error occured");}
     else{
       
     var formattedArr = [];
     var arrLen = resultsArray.length;
-    
+   
     for(var i = 0; i<arrLen; i++){
       formattedArr.push({
-      url: resultsArray[i].pagemap.cse_image[i].src ,
-      snippet: resultsArray[i].snippet,
-      context: resultsArray[i].link,
-      thumbnail: resultsArray[i].pagemap.cse_thumbnail[i].src 
+      url: resultsArray[i].pagemap.cse_image[i].src 
+      //snippet: resultsArray[i].snippet,
+      //context: resultsArray[i].link,
+      //thumbnail: resultsArray[i].pagemap.cse_thumbnail[i].src 
       })
       console.log(formattedArr[i]);
     }
   
     resolve(formattedArr);
-    
     }
-  });
-}
+
+
+
+
+*/
